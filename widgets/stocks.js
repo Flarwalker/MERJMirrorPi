9643ac805692c72b6cc4e428d6ce4c9190dd1b3f
Eric Weiss <eweiss1995@gmail.com>
2017-12-04 21:42:17 -0500

    Fixed stocks widget, added some font styling

--- widgets/stocks.js
+++ widgets/stocks.js
@@ -1,24 +1,36 @@
-stockDivs ();
+stockDivs();
 
-function stockDivs () {
-    $('Stock').html('<div id = "open"></div>');
+function stockDivs() {
+    $('#Stocks').html('<div id ="stocks">' +
+        '<div id="stock_title"></div>' +
+        '<div id="open"></div>' +
+        '<div id="high"></div>' +
+        '<div id="low"></div>' +
+        '<div id="close"></div>' +
+        '</div>');
 
     stockSet();
     setInterval('stockSet()', 600000);
 }
 
-function stockSet () {
-    $.get('/stocks', function(ret) {
-       var stockname = ret.stockname;
+function stockSet() {
+    $.get('/layout', function (ret) {
+        var stockname = ret.stocks;
         $.ajax({
-            url:
-            "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stockname + "&apikey=D2BSH93RQ73806N5",
-            sucess: function (parsed_json) {
-                var curDay = today.getDate();
-                var curMonth = months[today.getMonth()];
-                var curYear = today.getFullYear();
+            type: 'GET',
+            url: "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stockname + "&apikey=D2BSH93RQ73806N5",
+            success: function (parsed_json) {
+                var date = new Date();
+                var currentdate;
 
-                var currentdate = curYear + "-" + curMonth + "-" + curDay;
+                date.getDate();
+
+                currentdate =
+                    date.getFullYear() +
+                    "-" +
+                    ("0" + (date.getMonth() + 1)).slice(-2) +
+                    "-" +
+                    ("0" + date.getDate()).slice(-2);
 
                 var stockname = parsed_json['Meta Data']['2. Symbol'];
                 var open = parsed_json['Time Series (Daily)'][currentdate]['1. open'];
@@ -26,11 +38,15 @@ function stockSet () {
                 var low = parsed_json['Time Series (Daily)'][currentdate]['3. low'];
                 var close = parsed_json['Time Series (Daily)'][currentdate]['4. close'];
 
-
+                $("#stock_title").html("Stock: " + stockname);
+                $("#open").html("Open: " + open);
+                $("#high").html("High: " + high);
+                $("#low").html("Low: " + low);
+                $("#close").html("Close: " + close);
+            },
+            error: function () {
+                console.log("API Offline");
             }
-        })
-        error: function( {
-            console.log("API Offline")
-        })
+        });
     });
 }
